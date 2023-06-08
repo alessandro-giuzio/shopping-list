52fd75e44ca1081e172fae628a63bd65a9d3f2b9
alessandro-giuzio <giuzio@icloud.com>
2023-06-08 10:20:15 +0200

    prevent duplicate items

--- script.js
+++ script.js
@@ -33,6 +33,11 @@ function onAddItemSubmit(e){
     itemToEdit.classList.remove('.edit-mode');
     itemToEdit.remove();
     isEditMode = false;
+  }else{
+    if(checkIfItemExists(newItem)){
+      alert('Item already exists');
+      return
+    }
   }
 
 
@@ -98,6 +103,12 @@ function onClickItem(e){
   }else{
     setItemToEdit(e.target)
   }
+}
+
+function checkIfItemExists(item){
+  const itemsFromStorage = getItemsFromStorage();
+
+  return itemsFromStorage.includes(item);
 }
 
 function  setItemToEdit (item){
